8e1d36b5d2c603276eeb1df31716c78ccc253e34
LeGlutux <lp.bendeks@gmail.com>
2020-06-06 02:44:34 +0200

    pour faire plaisir

--- src/app/mining-dice/mining-dice.component.ts
+++ src/app/mining-dice/mining-dice.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { Rocks } from './rocks.model';
 
 @Component({
@@ -6,13 +6,21 @@ import { Rocks } from './rocks.model';
   templateUrl: './mining-dice.component.html',
   styleUrls: ['./mining-dice.component.scss']
 })
-export class MiningDiceComponent implements OnInit {
+export class MiningDiceComponent {
 
-  constructor() { }
+  public loot: Rocks[] = [];
 
-  ngOnInit(): void {
+  public charbon() {
+    return this.loot.filter((e) => e.name === 'charbon').length;
   }
 
+  public quartz() {
+    return this.loot.filter((e) => e.name === 'quartz').length;
+  }
+
+  public topaze() {
+    return this.loot.filter((e) => e.name === 'topaze').length;
+  }
 
   public handleRandom(prof: number) {
 
@@ -22,17 +30,18 @@ export class MiningDiceComponent implements OnInit {
   }
 
   public loots(collectNumbers: number, prof: number) {
-    const data = [];
-
+    this.loot = [];
     for (let i = 0; i < collectNumbers; i++) {
-      data.push(this.repart(prof));
+      this.loot.push(this.repart(prof));
     }
-    console.log(data);
+    return this.loot;
   }
 
   public repart(prof: number) {
     const rocksType: Rocks[] = [{ name: 'charbon', top: 14 }, { name: 'quartz', top: 19 }, { name: 'topaze', top: 60 }];
     const possibleRocks = rocksType.filter((rock) => rock.top >= this.handleRandom(prof));
+    console.log(possibleRocks[0]);
     return possibleRocks[0];
+
   }
 }
